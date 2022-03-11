b81c2cab4a1a6c90e7e7b0f4b91035b41a1d1e5d
  <>
2022-03-11 17:07:20 +0200

    Piirtää laatikon, mutta vääristä pisteistä.

--- prf_maa_laatikko_plot.py
+++ prf_maa_laatikko_plot.py
@@ -1,29 +1,44 @@
-#!/usr/bin/python3.9
+#!/usr/bin/python3
 import pandas as pd
 import xarray as xr
 import numpy as np
 from matplotlib.pyplot import *
-import sys
+import sys, warnings
 import prf_extent as prf
 import talven_ajankohta as taj
 import maalajit as ml
 
 ikir_ind = 0
+verbose = False
 
-def vaihda_ikirluokka(hyppy:int,data):
-    global ikir_ind # myös data, ax ja ikirluokat ovat pääfunktiosta
-    ikir_ind = ( ikir_ind + len(data) + hyppy ) % len(data)
+vari1 = '\033[1;32m'
+vari2 = '\033[1;93m'
+vari0 = '\033[0m'
+
+def testikartta(dt):
+    import cartopy.crs as ccrs
+    ax = axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
+    maadf0 = maadf.where(maadf == 0, 0)
+    maadf0 = maadf0.iloc[:,0]
+    dt0 = dt.where(dt == 0, 0)
+    dt0 = dt0.iloc[:,0]
+    show()
+
+def vaihda_ikirluokka(hyppy:int,dflista):
+    global ikir_ind # myös dflista, ax ja ikirluokat ovat pääfunktiosta
+    ikir_ind = ( ikir_ind + len(dflista) + hyppy ) % len(dflista)
     ax.clear()
-    data[ikir_ind].boxplot( whis=(5,95), ax=ax )
+    dflista[ikir_ind].boxplot( whis=(5,95), ax=ax )
     ax.set_ylim(mmin,mmax)
     title(ikirluokat[ikir_ind])
     draw()
 
 def nappainfunk(tapaht):
     if tapaht.key == 'right':
-        vaihda_ikirluokka(1,data)
+        vaihda_ikirluokka(1,dflista)
     elif tapaht.key == 'left':
-        vaihda_ikirluokka(-1,data)
+        vaihda_ikirluokka(-1,dflista)
 
 if __name__ == '__main__':
     rcParams.update({'font.size':13,'figure.figsize':(12,10)})
@@ -37,16 +52,23 @@ if __name__ == '__main__':
             startend = 'end'
         elif a == '-o' or a == '--osuusraja':
             osuusraja = float(argv[i+1])
-            
-    turha,maa = ml.lue_maalajit(ml.tunnisteet.keys())
-    maa = ml.maalajien_yhdistamiset(maa,pudota=True)
-    maadf = maa.to_dataframe(dim_order=['lat','lon'])
-    ml.nimen_jako(maadf)
+        elif a == '-v':
+            verbose = True
     
-    doy = taj.lue_doyt(startend).mean(dim='time')
+    turha,maa = ml.lue_maalajit(ml.tunnisteet.keys()) #xr.DataSet (lat,lon)
+    maa = ml.maalajien_yhdistamiset(maa,pudota=True)
+    maadf = maa.to_dataframe() #index = lat,lon
+    with open('tmp.txt','w') as f:
+        f.write(maadf.to_string())
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings( action='ignore', message='Mean of empty slice' )
+        doy = taj.lue_doyt(startend).mean(dim='time')
     maadf.where( maadf >= osuusraja, np.nan, inplace=True )
     maadf.where( maadf != maadf,     1,      inplace=True )
-    print(maadf.sum(axis='index').astype(int))
+    if verbose:
+        print('\n'+vari1+'Datapisteitä:'+vari0)
+        print(maadf.sum(axis='index').astype(int))
     maadf = maadf.mul( doy.data.flatten(), axis='index' )
     jarj = maadf.median().to_numpy().argsort() #järjestetään maalajit mediaanin mukaan
     if startend == 'end':
@@ -56,22 +78,30 @@ if __name__ == '__main__':
     ikirouta = prf.Prf('1x1').rajaa( (doy.lat.min(), doy.lat.max()+1) ) #pitäisi olla 25km, koska tämä vääristää tuloksia
     ikirstr = prf.luokittelu_str(np.mean(ikirouta.data,axis=0)) #np.2Darray
 
+    ml.nimen_jako(maadf)
     ikirluokat = prf.luokat[1:] #distinguishing_isolated puuttuu datasta
-    data = np.empty(len(ikirluokat),object)
+    dflista = np.empty(len(ikirluokat),object)
     for i,ikirluok in enumerate(ikirluokat):
         a = ikirstr.flatten()==ikirluok
-        data[i] = maadf.loc[a,:]
+        dflista[i] = maadf.loc[a,:].reset_index()
+    #testikartta(dflista[-1])
     mmin = np.inf
     mmax = -np.inf
-    for d in data:
-        mmin = min((min(d.min()),mmin))
-        mmax = max((max(d.max()),mmax))
+    for df in dflista:
+        mmin = min((min(df.min()),mmin))
+        mmax = max((max(df.max()),mmax))
     mmin = np.floor(mmin/10)*10
     mmax = np.ceil(mmax/10)*10
+    if verbose:
+        print('min/max:',mmin,mmax)
+        print('\n'+vari1+'Datapisteitä ikiroutaluokissa:'+vari0,end='')
+        for df,luok in zip(dflista,ikirluokat):
+            print('\n'+vari2+luok+vari0)
+            print(np.isfinite(df).sum())
     fig = figure()
-    ax = data[ikir_ind].boxplot( whis=(5,95) )
+    ax = dflista[ikir_ind].boxplot( whis=(5,95) )
     title(ikirluokat[ikir_ind])
-    #ax.set_ylim(mmin,mmax)
+    ax.set_ylim(mmin,mmax)
     tight_layout()
     if not tallenna:
         fig.canvas.mpl_connect('key_press_event',nappainfunk)
@@ -79,6 +109,6 @@ if __name__ == '__main__':
         exit()
     while 1:
         savefig('kuvia/prf_köppen_laatikko%i.png' %ikir_ind)
-        if(ikir_ind == len(data)-1):
+        if(ikir_ind == len(dflista)-1):
             exit()
         vaihda_ikirluokka(1)
