77d14d79a7c626b8f9c286e73aa9d732c0599db9
  <>
2022-03-14 08:17:36 +0200

    Keskiarvokartan piirtäminen päivitetty ja uudelleennimetty.

---
+++ prf_kartta_avg_plot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from matplotlib.pyplot import *
+import cartopy.crs as ccrs
+import matplotlib
+import prf_extent as prf
+
+def main():
+    platecarree = ccrs.PlateCarree()
+#    projektio   = ccrs.LambertAzimuthalEqualArea(central_latitude=90)
+    projektio  = platecarree
+    kattavuus   = [-180,180,25,90]
+    ax = axes(projection=projektio)
+    ax.coastlines()
+    ax.set_extent(kattavuus,platecarree)
+    ikirouta = prf.Prf('1x1',muoto='xarray').data.mean(dim='time')
+    ikirouta.plot.pcolormesh(transform=platecarree,cmap=get_cmap('rainbow'))
+    tight_layout()
+    show()
+
+if __name__ == '__main__':
+    rcParams.update({'font.size':13,'figure.figsize':(12,10)})
+    main()
--- prf_kartta_plot.py
+++ prf_kartta_plot.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
 import numpy as np
 from matplotlib.pyplot import *
-import cartopy.crs as ccrs
-import matplotlib
 import prf_extent as prf
+import cartopy.crs as ccrs
 
-def toinen():
-    ikirouta = prf.Prf('1x1').rajaa( (40,90) )
-    ikirdata = np.mean(ikirouta.data,axis=0)
-    ikirnum = prf.luokittelu_num(ikirdata)
-    ikirouta = None
-    ikirdata = None
-    luokkia=4
-    cmap = matplotlib.cm.get_cmap('plasma_r',lut=luokkia)
-    cmap = matplotlib.colors.ListedColormap([ cmap(i) for i in range(luokkia) ])
-    imshow(ikirnum, cmap=cmap, interpolation='none')
-    cbar=colorbar()
-    vali = (luokkia-1)/luokkia
-    cbar.set_ticks( np.linspace(vali/2,luokkia-1-vali/2,luokkia) )
-    cbar.ax.set_yticklabels(prf.luokat)
-    tight_layout()
-    show()
+def vaihda_vuosi(maara:int):
+    global vuosi_ind
+    vuosi_ind = (vuosi_ind + tif.data.shape[0] + maara) % tif.data.shape[0]
+    olio.set_array(tif.data[vuosi_ind,:,:].data.flatten())
+    title(str(tif.vuodet[vuosi_ind]))
+    draw()
 
-def main():
-    platecarree = ccrs.PlateCarree()
-    projektio   = ccrs.LambertAzimuthalEqualArea(central_latitude=90)
-#    projektio  = platecarree
-    kattavuus   = [-180,180,45,90]
-    ax = axes(projection=projektio)
-    ax.coastlines()
-    ax.set_extent(kattavuus,platecarree)
-    ikirouta = prf.Prf('1x1',muoto='xarray').data.mean(dim='time')
-    ikirouta.plot.pcolormesh(transform=platecarree)
-    show()
+def nappainfunk(tapaht):
+    if tapaht.key == 'right' or tapaht.key == 'o' or tapaht.key == 'i':
+        vaihda_vuosi(1)
+    elif tapaht.key == 'left' or tapaht.key == 'a' or tapaht.key == 'g':
+        vaihda_vuosi(-1)
 
 if __name__ == '__main__':
     rcParams.update({'font.size':13,'figure.figsize':(12,10)})
-    main()
+    tif = prf.Prf('1x1','xarray')
+    vuosi_ind = 0
+    platecarree = ccrs.PlateCarree()
+    projektio = platecarree
+    fig = figure()
+    ax = axes(projection=projektio)
+    ax.coastlines()
+    olio = tif.data[vuosi_ind,:,:].plot.pcolormesh( cmap=get_cmap('rainbow'), ax=ax )
+    title(str(tif.vuodet[vuosi_ind]))
+    tight_layout()
+    fig.canvas.mpl_connect('key_press_event',nappainfunk)
+    show()
