5e6873395d8da096d2d015c0978bc9a482c93b46
  <>
2022-03-15 17:31:25 +0200

    Ikiroutatyypit eritellään nyt.

--- prf_histog_plot.py
+++ prf_histog_plot.py
@@ -21,6 +21,26 @@ def argumentit():
             tarkk = int(sys.argv[i])
     return
 
+def vaihda_ikirluokka(hyppy:int):
+    global ikir_ind
+    ikir_ind = ( ikir_ind + len(prf.luokat) + hyppy ) % len(prf.luokat)
+    if xarrlis[ikir_ind] is None:
+        xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1( doy.where(ikirstr==prf.luokat[ikir_ind],np.nan), tarkk )
+    for i,suorak in enumerate(palkit):
+        suorak.set_height(yarrlis[ikir_ind][i]/1000/tarkk)
+    title(prf.luokat[ikir_ind])
+    gca().relim()
+    gca().autoscale_view()
+    draw()
+    return
+
+def nappainfunk(tapaht):
+    if tapaht.key == 'right' or tapaht.key == 'o' or tapaht.key == 'i':
+        vaihda_ikirluokka(1)
+    elif tapaht.key == 'left' or tapaht.key == 'g' or tapaht.key == 'a':
+        vaihda_ikirluokka(-1)
+    return
+
 def pintaalat1x1(darr,tarkk):
     aste = 0.0174532925199
     R2 = 40592558970441
@@ -38,20 +58,26 @@ def pintaalat1x1(darr,tarkk):
             lukualat[(luku-minluku)//tarkk] += ala
     return np.arange(minluku,maxluku+1,tarkk), lukualat
 
-def piirra_histogrammi(darr): #xarray.DataArray
-    xarr,yarr = pintaalat1x1(darr,tarkk)
-    plot(xarr,yarr/1000/tarkk,'.')
-    xlabel('winter %s doy' %startend)
-    ylabel('extent (1000 km$^2$ / %s day)' %startend)
-    
-    tight_layout()
-    show()
-
 if __name__ == '__main__':
     argumentit()
+    ikir_ind = 0
     rcParams.update({'font.size':13,'figure.figsize':(12,10)})
     doy = taj.lue_avgdoy(startend)
     ikirouta = prf.Prf('1x1').rajaa( (doy.lat.min(), doy.lat.max()+1) )
     ikirstr = prf.luokittelu_str_xr(ikirouta.data.mean(dim='time'))
+    prf.luokat = prf.luokat[1:]
 
-    piirra_histogrammi(doy.where(ikirstr, np.nan))
+    xarrlis = [None]*len(prf.luokat)
+    yarrlis = xarrlis.copy()
+    xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1( doy.where(ikirstr==prf.luokat[ikir_ind],np.nan), tarkk )
+
+    fig = figure()
+    palkit = bar( xarrlis[ikir_ind], yarrlis[ikir_ind]/1000/tarkk, width=0.8*tarkk )
+    xlabel('winter %s doy' %startend)
+    ylabel('extent (1000 km$^2$ / %s day)' %startend)
+    title(prf.luokat[ikir_ind])
+    tight_layout()
+    if not tallenna:
+        fig.canvas.mpl_connect('key_press_event',nappainfunk)
+        show()
+        exit()
