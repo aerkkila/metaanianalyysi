a43fdeed418bda0f02bf7f7e0090fb0b496b28b0
  <>
2022-03-17 15:40:21 +0200

    lisätty tietojen tulostusmahdollisuus

--- prf_histog.py
+++ prf_histog.py
@@ -6,19 +6,23 @@ import talven_ajankohta as taj
 import prf_extent as prf
 
 def argumentit():
-    global startend, tallenna, tarkk
-    tallenna = False
-    startend = 'start'
-    tarkk = 1
-    for i in range(len(sys.argv)):
+    global startend, tallenna, tarkk, verbose
+    tallenna=False; startend='start'; tarkk=1; verbose=False
+    i = 1
+    while i < len(sys.argv):
         a = sys.argv[i]
         if a == '-s':
             tallenna = True
+        elif a == '-v':
+            verbose = True
         elif a == 'start' or a == 'end':
             startend = a
         elif a == '-t' or a == '--tarkkuus':
             i += 1
             tarkk = int(sys.argv[i])
+        else:
+            print("Varoitus: tuntematon argumentti \"%s\"" %a)
+        i += 1
     return
 
 def vaihda_ikirluokka(hyppy:int):
@@ -26,6 +30,11 @@ def vaihda_ikirluokka(hyppy:int):
     ikir_ind = ( ikir_ind + len(prf.luokat) + hyppy ) % len(prf.luokat)
     if xarrlis[ikir_ind] is None:
         xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1( doy.where(ikirstr==prf.luokat[ikir_ind],np.nan), tarkk )
+        return True
+    return False
+
+def vaihda_ikirluokka_piirtaen(hyppy:int):
+    vaihda_ikirluokka(hyppy)
     for i,suorak in enumerate(palkit):
         suorak.set_height(yarrlis[ikir_ind][i]/1000/tarkk)
     title(prf.luokat[ikir_ind])
@@ -36,11 +45,13 @@ def vaihda_ikirluokka(hyppy:int):
 
 def nappainfunk(tapaht):
     if tapaht.key == 'right' or tapaht.key == 'o' or tapaht.key == 'i':
-        vaihda_ikirluokka(1)
+        vaihda_ikirluokka_piirtaen(1)
     elif tapaht.key == 'left' or tapaht.key == 'g' or tapaht.key == 'a':
-        vaihda_ikirluokka(-1)
+        vaihda_ikirluokka_piirtaen(-1)
     return
 
+# Diskretisoi darr-DataArrayn tarkk-tarkkuudella ja
+# laskee paljonko pinta-alaa on kullakin syntyneellä osuudella.
 def pintaalat1x1(darr,tarkk):
     aste = 0.0174532925199
     R2 = 40592558970441
@@ -71,6 +82,15 @@ if __name__ == '__main__':
     yarrlis = xarrlis.copy()
     xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1( doy.where(ikirstr==prf.luokat[ikir_ind],np.nan), tarkk )
 
+    if verbose:
+        import locale
+        locale.setlocale(locale.LC_ALL,'')
+        tulosta = lambda : print(locale.format_string( 'Yhteensä "%s"-aluetta %.2f Mkm²',
+                                                       (prf.luokat[ikir_ind], np.sum(yarrlis[ikir_ind])*1e-6) ))
+        tulosta()
+        while vaihda_ikirluokka(1):
+            tulosta()
+
     fig = figure()
     palkit = bar( xarrlis[ikir_ind], yarrlis[ikir_ind]/1000/tarkk, width=0.8*tarkk )
     xlabel('winter %s day' %startend)
@@ -85,4 +105,4 @@ if __name__ == '__main__':
         savefig("kuvia/%s_%s%i.png" %(sys.argv[0][:-3],startend,ikir_ind))
         if ikir_ind+1 == len(prf.luokat):
             exit()
-        vaihda_ikirluokka(1)
+        vaihda_ikirluokka_piirtaen(1)
