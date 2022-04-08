4082c28a078d890b01f5fc3772c187f26ea4fb83
  <>
2022-04-08 16:22:22 +0300

    selkeytetty

--- bawld_vuot_rajaus.py
+++ bawld_vuot_rajaus.py
@@ -8,15 +8,15 @@ def wlvuo(maski):
     return vuo[maski]/wl[maski]
 
 ei_paivit__ = False
-def paivita_wetlandraja(arvo):
-    global wetlandraja,ei_paivit__
+def paivita_raja_wl_kuiva(arvo):
+    global raja_wl_kuiva,ei_paivit__
     if ei_paivit__:
         return
     ei_paivit__ = True
     liuku.set_val(arvo)
     ei_paivit__ = False
-    wetlandraja = arvo
-    maski = (wl<wetlandraja) & (luokmaski)
+    raja_wl_kuiva = arvo
+    maski = (wl<raja_wl_kuiva) & (luokmaski)
     viivat.set_xdata(osuus[maski])
     viivat.set_ydata(vuo[maski])
     avg = np.mean(vuo[maski])
@@ -25,10 +25,10 @@ def paivita_wetlandraja(arvo):
     teksti.set_y(avg)
     draw()
 
-def paivita_osuusraja_wl(arvo):
-    global osuusraja_wl
-    osuusraja_wl = arvo
-    maski = (wl>=osuusraja_wl)
+def paivita_raja_wl_wl(arvo):
+    global raja_wl_wl
+    raja_wl_wl = arvo
+    maski = (wl>=raja_wl_wl)
     v = wlvuo(maski)
     viivat_wl.set_xdata(wl[maski])
     viivat_wl.set_ydata(v)
@@ -40,31 +40,32 @@ def paivita_osuusraja_wl(arvo):
 
 def painettaessa(tapaht):
     if tapaht.key == 'down' or tapaht.key == 'a':
-        paivita_wetlandraja(wetlandraja-0.01)
+        paivita_raja_wl_kuiva(raja_wl_kuiva-0.01)
     elif tapaht.key == 'up' or tapaht.key == 'i':
-        paivita_wetlandraja(wetlandraja+0.01)
+        paivita_raja_wl_kuiva(raja_wl_kuiva+0.01)
     elif tapaht.key == 'left' or tapaht.key == 'g':
-        paivita_wetlandraja(wetlandraja-0.001)
+        paivita_raja_wl_kuiva(raja_wl_kuiva-0.001)
     elif tapaht.key == 'right' or tapaht.key == 'o':
-        paivita_wetlandraja(wetlandraja+0.001)
+        paivita_raja_wl_kuiva(raja_wl_kuiva+0.001)
     else:
         return
 
 def main():
-    global viivat,avgviiva,viivat_wl,avgviiva_wl,wl,vuo,luokmaski,osuus,wetlandraja,osuusraja_wl,liuku
+    global viivat,avgviiva,viivat_wl,avgviiva_wl,wl,vuo,luokmaski,osuus,raja_wl_kuiva,raja_wl_wl,liuku
     global teksti, teksti_wl
     locale.setlocale(locale.LC_ALL,'')
-    luokka='boreal_forest'
+    kuivaluokka='tundra_dry'
     wlluokka='wetland'
-    osuusraja = 0.05
-    wetlandraja = 0.010
-    osuusraja_wl = osuusraja
+    raja_kuiva_kuiva = 0.2
+    raja_wl_kuiva = 0.010
+    raja_wl_wl = 0.05
+    
     vuot = xr.open_dataarray(config.edgartno_dir+'posterior.nc').mean(dim='time')
     baw = xr.open_dataset('BAWLD1x1.nc')
 
     vuo = vuot.sel({'lat':slice(baw.lat.min(), baw.lat.max())}).data.flatten()
-    osuus = baw[luokka].data.flatten()
-    luokmaski = (osuus>=osuusraja)
+    osuus = baw[kuivaluokka].data.flatten()
+    luokmaski = (osuus>=raja_kuiva_kuiva)
     if not any(luokmaski):
         return
     wl = baw[wlluokka].data.flatten()
@@ -73,17 +74,19 @@ def main():
     baw.close()
     ax = axes([0.08,0.07,0.8,0.77])
 
-    maski = (wl<wetlandraja) & (luokmaski)
-    viivat, = plot(osuus[maski], vuo[maski], '.', label=luokka, color='b')
+    #kuiva luokka
+    maski = (wl<raja_wl_kuiva) & (luokmaski)
+    viivat, = plot(osuus[maski], vuo[maski], '.', label=kuivaluokka, color='b')
     avg = np.mean(vuo[maski])
-    avgviiva, = plot([osuusraja,1], [avg]*2, color='deepskyblue')
+    avgviiva, = plot([raja_kuiva_kuiva,1], [avg]*2, color='deepskyblue')
     teksti = text(1.02,avg,locale.format_string('σ = %.2e', avg))
 
-    maski = (wl>=osuusraja_wl)
+    #märkä luokka
+    maski = (wl>=raja_wl_wl)
     v = wlvuo(maski)
     viivat_wl, = plot(wl[maski], v, 'x', label=wlluokka, color='r')
     avg = np.mean(v)
-    avgviiva_wl, = plot([osuusraja,1], [avg]*2, color='lightsalmon')
+    avgviiva_wl, = plot([raja_kuiva_kuiva,1], [avg]*2, color='lightsalmon')
     teksti_wl = text(1.02,avg,locale.format_string('σ = %.2e', avg))
 
     grid('on')
@@ -98,11 +101,11 @@ def main():
         ax      = axes([0.15, 0.93, 0.72, 0.04]),
         label   = "%s <" %wlluokka,
         valmin  = 0,
-        valmax  = 1-osuusraja,
+        valmax  = 1-raja_kuiva_kuiva,
         valfmt  = '%.3f',
-        valinit = wetlandraja
+        valinit = raja_wl_kuiva
     )
-    liuku.on_changed(paivita_wetlandraja)
+    liuku.on_changed(paivita_raja_wl_kuiva)
     gcf().canvas.mpl_connect('key_press_event', painettaessa)
     wlliuku = Slider(
         ax      = axes([0.15, 0.87, 0.72, 0.04]),
@@ -110,9 +113,9 @@ def main():
         valmin  = 0,
         valmax  = 1,
         valfmt  = '%.3f',
-        valinit = osuusraja_wl
+        valinit = raja_wl_wl
     )
-    wlliuku.on_changed(paivita_osuusraja_wl)
+    wlliuku.on_changed(paivita_raja_wl_wl)
     show()
 
 if __name__ == '__main__':
