29f6af09c4c63c5aa6059a656298640023e1dbd6
  <>
2022-04-11 12:53:44 +0300

    Lisätty liukuja

--- bawld_vuot_rajaus.py
+++ bawld_vuot_rajaus.py
@@ -4,90 +4,90 @@ import numpy as np
 from matplotlib.pyplot import *
 import config, locale
 
-def wlvuo(maski):
-    return vuo[maski]/wl[maski]
+def luokan_vuo(maski,ind2):
+    return vuo[maski]#/osuus[ind2][maski]
 
-ei_paivit__ = False
-def paivita_raja_wl_kuiva(arvo):
-    global raja_wl_kuiva,ei_paivit__
-    if ei_paivit__:
-        return
-    ei_paivit__ = True
-    liuku.set_val(arvo)
-    ei_paivit__ = False
-    raja_wl_kuiva = arvo
-    maski = (wl<raja_wl_kuiva) & (luokmaski)
-    viivat.set_xdata(osuus[maski])
-    viivat.set_ydata(vuo[maski])
-    avg = np.mean(vuo[maski])
-    avgviiva.set_ydata([avg]*2)
-    teksti.set_text(locale.format_string('σ = %.2e', avg))
-    teksti.set_y(avg)
+def paivita_raja(ind2):
+    maski = luokmaski[ind2*2] & luokmaski[ind2*2+1]
+    v = luokan_vuo(maski,ind2)
+    viivat[ind2].set_xdata(osuus[ind2][maski])
+    viivat[ind2].set_ydata(v)
+    avg = np.mean(v)
+    avgviiva[ind2].set_ydata([avg]*2)
+    teksti[ind2].set_text(locale.format_string('σ = %.2e', avg))
+    teksti[ind2].set_y(avg)
     draw()
+    return
+
+def paivita_raja_kuiva_kuiva(arvo):
+    rajat[0] = arvo
+    luokmaski[0] = (osuus[0]>=rajat[0])
+    paivita_raja(0)
+    return
+
+def paivita_raja_wl_kuiva(arvo):
+    rajat[1] = arvo
+    luokmaski[1] = (osuus[1]<rajat[1])
+    paivita_raja(0)
+    return
 
 def paivita_raja_wl_wl(arvo):
-    global raja_wl_wl
-    raja_wl_wl = arvo
-    maski = (wl>=raja_wl_wl)
-    v = wlvuo(maski)
-    viivat_wl.set_xdata(wl[maski])
-    viivat_wl.set_ydata(v)
-    avg = np.mean(v)
-    avgviiva_wl.set_ydata([avg]*2)
-    teksti_wl.set_text(locale.format_string('σ = %.2e', avg))
-    teksti_wl.set_y(avg)
-    draw()
+    rajat[2] = arvo
+    luokmaski[2] = (osuus[1]>=rajat[2])
+    paivita_raja(1)
+    return
 
-def painettaessa(tapaht):
-    if tapaht.key == 'down' or tapaht.key == 'a':
-        paivita_raja_wl_kuiva(raja_wl_kuiva-0.01)
-    elif tapaht.key == 'up' or tapaht.key == 'i':
-        paivita_raja_wl_kuiva(raja_wl_kuiva+0.01)
-    elif tapaht.key == 'left' or tapaht.key == 'g':
-        paivita_raja_wl_kuiva(raja_wl_kuiva-0.001)
-    elif tapaht.key == 'right' or tapaht.key == 'o':
-        paivita_raja_wl_kuiva(raja_wl_kuiva+0.001)
-    else:
-        return
+def paivita_raja_yht_wl(arvo):
+    rajat[3] = arvo
+    luokmaski[3] = (osuus[0]+osuus[1]>=rajat[3])
+    paivita_raja(1)
+    return
 
 def main():
-    global viivat,avgviiva,viivat_wl,avgviiva_wl,wl,vuo,luokmaski,osuus,raja_wl_kuiva,raja_wl_wl,liuku
-    global teksti, teksti_wl
+    global viivat,avgviiva,teksti,vuo,luokmaski,osuus,rajat
     locale.setlocale(locale.LC_ALL,'')
     kuivaluokka='tundra_dry'
     wlluokka='wetland'
-    raja_kuiva_kuiva = 0.2
-    raja_wl_kuiva = 0.010
-    raja_wl_wl = 0.05
+    raja_kuiva_kuiva = 0.2 #vähintään kuivaa kuivassa
+    raja_wl_kuiva = 0.010 #alle tämän verran märkää kuivassa
+    raja_wl_wl = 0.05 #vähintään märkää märässä
+    raja_yht_wl = 0.8 #vähintään näin paljon yhteensä kuiva- ja märkäluokkaa
+    rajat = [raja_kuiva_kuiva, raja_wl_kuiva, raja_wl_wl, raja_yht_wl]
     
     vuot = xr.open_dataarray(config.edgartno_dir+'posterior.nc').mean(dim='time')
     baw = xr.open_dataset('BAWLD1x1.nc')
 
     vuo = vuot.sel({'lat':slice(baw.lat.min(), baw.lat.max())}).data.flatten()
-    osuus = baw[kuivaluokka].data.flatten()
-    luokmaski = (osuus>=raja_kuiva_kuiva)
-    if not any(luokmaski):
-        return
+    kuiva = baw[kuivaluokka].data.flatten()
     wl = baw[wlluokka].data.flatten()
+    osuus = [kuiva,wl]
+    luokmaski = [(kuiva>=raja_kuiva_kuiva), (wl<raja_wl_kuiva),
+                 (wl>=raja_wl_wl), (kuiva+wl>=raja_yht_wl)]
+    if not any(luokmaski[0]):
+        return
 
     vuot.close()
     baw.close()
-    ax = axes([0.08,0.07,0.8,0.77])
+    ax = axes([0.08,0.07,0.8,0.72])
 
     #kuiva luokka
-    maski = (wl<raja_wl_kuiva) & (luokmaski)
-    viivat, = plot(osuus[maski], vuo[maski], '.', label=kuivaluokka, color='b')
+    maski = (luokmaski[0]) & (luokmaski[1])
+    viivat_k, = plot(kuiva[maski], vuo[maski], '.', label=kuivaluokka, color='b')
     avg = np.mean(vuo[maski])
-    avgviiva, = plot([raja_kuiva_kuiva,1], [avg]*2, color='deepskyblue')
-    teksti = text(1.02,avg,locale.format_string('σ = %.2e', avg))
+    avgviiva_k, = plot([0.02, 0.98], [avg]*2, color='deepskyblue')
+    teksti_k = text(1.02,avg,locale.format_string('σ = %.2e', avg))
 
     #märkä luokka
-    maski = (wl>=raja_wl_wl)
-    v = wlvuo(maski)
-    viivat_wl, = plot(wl[maski], v, 'x', label=wlluokka, color='r')
+    maski = (luokmaski[2]) & (luokmaski[3])
+    v = luokan_vuo(maski,1)
+    viivat_w, = plot(wl[maski], v, 'x', label=wlluokka, color='r')
     avg = np.mean(v)
-    avgviiva_wl, = plot([raja_kuiva_kuiva,1], [avg]*2, color='lightsalmon')
-    teksti_wl = text(1.02,avg,locale.format_string('σ = %.2e', avg))
+    avgviiva_w, = plot([0.02, 0.98], [avg]*2, color='lightsalmon')
+    teksti_w = text(1.02,avg,locale.format_string('σ = %.2e', avg))
+
+    viivat = [viivat_k, viivat_w]
+    avgviiva = [avgviiva_k, avgviiva_w]
+    teksti = [teksti_k, teksti_w]
 
     grid('on')
     ymax = v.max()*0.66666
@@ -95,27 +95,44 @@ def main():
     ylim([ymin,ymax])
     ylabel('CH$_4$ (mol/m$^2$/s)')
     xlabel('osuus')
-    legend(frameon=True)
+    legend(loc='center right', frameon=True)
     
-    liuku = Slider(
-        ax      = axes([0.15, 0.93, 0.72, 0.04]),
-        label   = "%s <" %wlluokka,
-        valmin  = 0,
-        valmax  = 1-raja_kuiva_kuiva,
-        valfmt  = '%.3f',
-        valinit = raja_wl_kuiva
-    )
-    liuku.on_changed(paivita_raja_wl_kuiva)
-    gcf().canvas.mpl_connect('key_press_event', painettaessa)
-    wlliuku = Slider(
-        ax      = axes([0.15, 0.87, 0.72, 0.04]),
-        label   = "%s >=" %wlluokka,
+    liuku_kk = Slider(
+        ax      = axes([0.25, 0.96, 0.66, 0.02]),
+        label   = "kuivaa kuivassa >=",
         valmin  = 0,
         valmax  = 1,
         valfmt  = '%.3f',
-        valinit = raja_wl_wl
+        valinit = rajat[0]
     )
-    wlliuku.on_changed(paivita_raja_wl_wl)
+    liuku_kk.on_changed(paivita_raja_kuiva_kuiva)
+    liuku_wk = Slider(
+        ax      = axes([0.25, 0.88, 0.66, 0.02]),
+        label   = "märkää kuivassa <",
+        valmin  = 0,
+        valmax  = 1,
+        valfmt  = '%.3f',
+        valinit = rajat[1]
+    )
+    liuku_wk.on_changed(paivita_raja_wl_kuiva)
+    liuku_ww = Slider(
+        ax      = axes([0.25, 0.92, 0.66, 0.02]),
+        label   = "märkää märässä >=",
+        valmin  = 0,
+        valmax  = 1,
+        valfmt  = '%.3f',
+        valinit = rajat[2]
+    )
+    liuku_ww.on_changed(paivita_raja_wl_wl)
+    liuku_yw = Slider(
+        ax      = axes([0.25, 0.84, 0.66, 0.02]),
+        label   = "yhteensä märässä >=",
+        valmin  = 0,
+        valmax  = 1,
+        valfmt  = '%.3f',
+        valinit = rajat[3]
+    )
+    liuku_yw.on_changed(paivita_raja_yht_wl)
     show()
 
 if __name__ == '__main__':
