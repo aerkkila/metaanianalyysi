bbd093f21b2f0252099863159e837a4ae9d891ba
  <>
2022-04-29 14:02:23 +0300

    wetlandvuo_data omana koodinaan

---
+++ wetlandvuo_data.py
@@ -0,0 +1,50 @@
+import numpy as np
+import xarray as xr
+from copy import copy
+
+def tee_data(muoto='numpy'):
+    tallennusnimi = 'wetlandvuo_data.npz'
+    if muoto=='numpy':
+        try:
+            dt1 = np.load(tallennusnimi)
+            dt = deepcopy(dt1)
+            dt1.close()
+            return [dt['x'], dt['y'], dt['nimet']]
+        except:
+            pass
+    raja_wl = 0.2
+    dsbaw = xr.open_dataset('./BAWLD1x1.nc')\
+        [['wetland','bog','fen','marsh','tundra_wetland','permafrost_bog']]
+    dsbaw = xr.where(dsbaw.wetland>=raja_wl, dsbaw, np.nan)
+    dsvuo = xr.open_dataarray('./flux1x1_whole_year.nc').mean(dim='time').\
+        sel({'lat':slice(dsbaw.lat.min(), dsbaw.lat.max())})
+    dsvuo *= 1e9
+
+    if muoto=='numpy':
+        ds = dsbaw.drop_vars('wetland')
+        dt = np.empty([len(ds.keys()), ds[list(ds.keys())[0]].data.size])
+        for i,key in enumerate(ds.keys()):
+            dt[i,:] = ds[key].data.flatten()
+        dt = dt.transpose()
+        dty = dsvuo.data.flatten()
+        lasku = 0
+        #dropna
+        for i in range(dt.shape[0]):
+            if all(dt[i,:] == dt[i,:]) and dty[i] == dty[i]:
+                lasku += 1
+        uusix = np.empty([lasku, dt.shape[1]])
+        uusiy = np.empty(lasku)
+        ind = 0
+        for i in range(dt.shape[0]):
+            if all(dt[i,:] == dt[i,:]) and dty[i] == dty[i]:
+                uusix[ind,:] = dt[i,:]
+                uusiy[ind] = dty[i]
+                ind += 1
+        np.savez(tallennusnimi, x=uusix, y=uusiy, nimet=list(ds.keys()))
+        return [uusix, uusiy, list(ds.keys())]
+
+    #pandas tästä eteenpäin
+    ds = dsbaw.drop_vars('wetland').assign({dsvuo.name:dsvuo})
+    df = ds.to_dataframe()
+    df.dropna(how='any', inplace=True)
+    return df.reset_index(drop=True)
--- wetlandvuo_voting.py
+++ wetlandvuo_voting.py
@@ -1,57 +1,66 @@
 #!/usr/bin/python3
 from sklearn import linear_model
 import numpy as np
-import xarray as xr
 from voting_model import Voting
+import wetlandvuo_data as wldata
+import time
 
-def tee_data(muoto='pandas'):
-    raja_wl = 0.25
-    dsbaw = xr.open_dataset('./BAWLD1x1.nc')\
-        [['wetland','bog','fen','marsh','tundra_wetland','permafrost_bog']]
-    dsbaw = xr.where(dsbaw.wetland>=raja_wl, dsbaw, np.nan)
-    dsvuo = xr.open_dataarray('./flux1x1_whole_year.nc').mean(dim='time').\
-        sel({'lat':slice(dsbaw.lat.min(), dsbaw.lat.max())})
-    dsvuo *= 1e9
-    ds = dsbaw.drop_vars('wetland').assign({dsvuo.name:dsvuo})
+def taita_sarja(x, y, n_taitteet, n_sij):
+    pit = len(y) // n_taitteet
+    if(n_sij != n_taitteet-1):
+        validx = x[pit*n_sij : pit*(n_sij+1), ...]
+        validy = y[pit*n_sij : pit*(n_sij+1), ...]
+        harjx = np.concatenate((x[:pit*n_sij, ...], x[pit*(n_sij+1):]))
+        harjy = np.concatenate((y[:pit*n_sij, ...], y[pit*(n_sij+1):]))
+    else:
+        validx = x[pit*n_sij:]
+        validy = y[pit*n_sij:]
+        harjx = x[:pit*n_sij]
+        harjy = y[:pit*n_sij]
+    return harjx, harjy, validx, validy
 
-    if muoto=='numpy':
-        dt = np.empty([len(ds.keys()), ds[list(ds.keys())[0]].data.size])
-        for i,key in enumerate(ds.keys()):
-            dt[i,:] = ds[key].data.flatten()
-        dt = dt.transpose()
-        lasku = 0
-        #dropna
-        for i in range(dt.shape[0]):
-            if all(dt[i,:] == dt[i,:]):
-                lasku += 1
-        uusi = np.empty([lasku, dt.shape[1]])
-        ind = 0
-        for i in range(dt.shape[0]):
-            if all(dt[i,:] == dt[i,:]):
-                uusi[ind,:] = dt[i,:]
-                ind += 1
-        return uusi
-
-    #pandas tästä eteenpäin
-    df = ds.to_dataframe()
-    df.dropna(how='any', inplace=True)
-    return df.reset_index(drop=True)
+def ristivalidointidata(x, y, malli, n_taitteet, ennusrajat=()):
+    if(n_taitteet > len(y)):
+        print("Varoitus n_taitteet (%i) > len(df) (%i). n_taitteet muutetaan kokoon len(df)" %(n_taitteet,len(df.index())))
+        n_taitteet = len(y)
+    yhatut = np.empty(len(y))
+    ennushatut = np.empty([len(y), len(ennusrajat)])
+    print('')
+    ind = 0
+    alku = time.process_time()
+    for ind_taite in range(n_taitteet):
+        harjx,harjy,validx,validy = taita_sarja(x, y, n_taitteet, ind_taite)
+        malli.fit(harjx, harjy)
+        ind1 = ind+len(validy)
+        yhatut[ind:ind1] = malli.predict(validx)
+        for i,e in enumerate(ennusrajat):
+            ennushatut[ind:ind1,i] = malli.prediction(e)
+        print("\033[F%i/%i\033[K" %(ind_taite+1,n_taitteet))
+    print("\033[F\033[K", end='')
+    return yhatut, ennushatut, time.process_time()-alku
 
 def main():
     np.random.seed(12345)
     tyyppi = 'numpy'
-    dt = tee_data(tyyppi)
+    dt = wldata.tee_data(tyyppi)
     if tyyppi == 'pandas':
         datax = dt.drop('vuo', axis=1)
         datay = dt.vuo
     elif tyyppi == 'numpy':
-        datax = dt[:,:-1]
-        datay = dt[:,-1]
-    vm = Voting(linear_model.LinearRegression(), tyyppi, samp_kwargs={'n':50})
-    vm.fit(datax, datay)
-    print(vm.predict(datax, palauta=True)[:10])
-    print(vm.prediction(50)[:10])
-    print(vm.prediction(90)[:10])
+        datax = dt[0]
+        datay = dt[1]
+        nimet = dt[2]
+
+    #datax = np.array([np.sum(datax, axis=1),]).transpose() #vain wetland
+
+    vm = Voting(linear_model.LinearRegression(), tyyppi, n_estimators=500, samp_kwargs={'n':100})
+    ennusrajat = (5,20,50,80,95)
+    yhatut, ehatut, aika = ristivalidointidata(datax, datay, vm, 8, ennusrajat)
+    print('aika = %.4f s' %aika)
+    varlin = np.mean((yhatut-np.mean(datay))**2)
+    var = np.var(datay)
+    print("selitetty osuus = %.4f\n"
+          "alkup. varianssi = %.4f" %(1-varlin/var,var))
     return 0
 
 if __name__=='__main__':
