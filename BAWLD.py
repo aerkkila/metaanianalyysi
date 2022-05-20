b4d16e1cb410a2bc16b7513c34ea60b8281aadaa
  <>
2022-05-20 09:08:32 +0300

    korjattu

--- BAWLD.py
+++ BAWLD.py
@@ -3,7 +3,7 @@ import maalajit as ml
 
 if __name__ == '__main__':
     maat = ml.lue_maalajit(ml.tunnisteet_kaikki.keys())
-    for maa,nimi in zip(maat, ['BAWLD1x1.nc', 'BAWLD05x05.nc']):
+    for maa,nimi in zip(maat, ['BAWLD05x05.nc', 'BAWLD1x1.nc']):
         ml.maalajien_yhdistamiset(maa)
         maa *= 0.01
         maa.to_netcdf(nimi)
--- prfdata_avg.py
+++ prfdata_avg.py
@@ -5,7 +5,8 @@ import xarray as xr
 if __name__ == '__main__':
     prfolio = Prf()
     prfdata = xr.concat(prfolio.data, dim='time').sel({'lat':slice(23.5,90)}).astype(np.float32)
-    avgluok = luokittelu_num_xr(prfdata.mean(dim='time'))
+    prfdata = prfdata.mean(dim='time')
+    avgluok = luokittelu_num_xr(prfdata)
     tietue = {'luokka':avgluok, 'osuus':prfdata}
     ds = xr.Dataset(tietue)
     ds.to_netcdf('%s.nc' %sys.argv[0][:-3])
