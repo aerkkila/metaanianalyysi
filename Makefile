f598fc144697ff03d6ea1ad72c52be0c2c50aace
  <aerkkila@users.noreply.github.com>
2022-06-09 13:58:16 +0300

    uuden jäätymisdatan muuttaminen kausien pituuksiksi

--- Makefile
+++ Makefile
@@ -20,3 +20,14 @@ kaudet2.nc: FT2kaudet.out
 	./FT2kaudet.out ../FT1x1_2.nc $@
 
 kaudet_uusi: kaudet1.nc kaudet2.nc
+
+kausista_pituudet.out: kausista_pituudet.c
+	gcc -g -Og -o $@ kausista_pituudet.c -lnctietue -lnetcdf -lpng -lSDL2
+
+kausien_pituudet1.nc: kausista_pituudet.out kaudet1.nc
+	./kausista_pituudet.out kaudet1.nc $@
+
+kausien_pituudet2.nc: kausista_pituudet.out kaudet2.nc
+	./kausista_pituudet.out kaudet2.nc $@
+
+kausien_pituudet: kausien_pituudet1.nc kausien_pituudet2.nc
---
+++ kausista_pituudet.c
@@ -0,0 +1,119 @@
+#include <nctietue/nctietue.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int resol;
+#define KESA 1
+#define TALVI 3
+
+time_t unixaika(int vuosi, int kuukausi, int paiva) {
+  struct tm aikatm = {.tm_year=vuosi, .tm_mon=kuukausi-1, .tm_mday=paiva};
+  return mktime(&aikatm);
+}
+
+char edellinen_eri(char* ptr, int ind) {
+  char luku = ptr[ind];
+  while(ind >= resol)
+    if(ptr[ind-=resol] != luku)
+      return ptr[ind];
+  return -1;
+}
+
+int paivia_tata(char* ptr, int ind, int maxind) {
+  int paivia = 1;
+  if(ind>=maxind)
+    return -1;
+  char arvo = ptr[ind];
+  while((ind+=resol) < maxind) {
+    if(ptr[ind] != arvo)
+      return paivia;
+    paivia++;
+  }
+  return -1;
+}
+
+/* Vuosi vaihtuu elokuun alussa seuraavaan, koska syksy luetaan seuraavaan vuoteen,
+ * koska jäätyminen voi myös alkaa vasta vuoden vaihduttua
+ * ja sulaminen ei ala elokuun alun jälkeen.
+ */
+int hae_vuosi(int vuosi0, int paivia) {
+  return vuosi0 + paivia/365 + 1; //toimii, koska tällä aikavälillä on riittävän vähän karkausvuosia
+}
+
+int main(int argc, char** argv) {
+  nct_vset vset;
+  time_t alkuaika, loppuaika;
+  int paivia_yht, varid, vuosi, kk, paiva;
+  if(argc < 2) {
+    puts("Ei luettavan nimeä");
+    return 1;
+  }
+  if(argc < 3) {
+    puts("Ei tallennusnimeä");
+    return 1;
+  }
+  nct_read_ncfile_gd(&vset, argv[1]);
+  resol = NCTDIM(vset,"lat").len * NCTDIM(vset,"lon").len;
+  varid = nct_get_varid(&vset, "time");
+  char* str = nct_get_att_text(&vset, varid, "units");
+  if(!str)
+    return 1;
+  if(!(str = strstr(str, "days since ")))
+    return 2;
+  str += strlen("days since ");
+  if(sscanf(str, "%d-%d-%d", &vuosi, &kk, &paiva)!=3)
+    return 3;
+  alkuaika = unixaika(vuosi, kk, paiva);
+  paivia_yht = NCTDIM(vset,"time").len;
+  //loppuaika = alkuaika + paivaaika*paivia_yht;
+  int pituuspituus = resol*(paivia_yht/365+1);
+  short* pituudet1[3];
+  for(int i=0; i<3; i++)
+    pituudet1[i] = calloc(pituuspituus,2);
+  for(varid=0; varid<vset.nvars; varid++)
+    if(!(vset.vars[varid].iscoordinate))
+      break;
+  
+  int vuosi0=vuosi, maxind=paivia_yht*resol;
+  if(maxind != nct_getlen(&vset, varid)) {
+    printf("saatiin maxind = %i, mutta pitäisi olla %li\n", maxind, nct_getlen(&vset, varid));
+    return 5;
+  }
+
+  for(int i=0; i<resol; i++) {
+    char* ptr = vset.vars[varid].data;
+    int ind=i, paivia;
+    while(ptr[ind+=resol] == KESA); // Siirrytään seuraavan kauden alkuun.
+    while((paivia = paivia_tata(ptr, ind, maxind)) >= 0) { // montako päivää on tätä kautta
+      int kausi = ptr[ind]; // mikä on tämä kausi
+      if(!kausi)
+	goto SEURAAVA;
+      if(kausi>TALVI || kausi < 0)
+	return 4;
+      if(paivia > 365)
+	paivia = 365; // Jos kausi kestää monta vuotta, laitetaan joka vuodelle 365.
+      vuosi = hae_vuosi(vuosi0, ind/resol);
+      pituudet1[kausi-1][(vuosi-vuosi0)*resol+i] = paivia;
+    SEURAAVA:
+      ind += paivia*resol; // seuraavan kauden alkuun
+    }
+  }
+  nct_vset tallenn = {0};
+  nct_add_coord(&tallenn, nct_range_NC_INT(vuosi0,vuosi+1,1), vuosi-vuosi0+1, NC_INT, "vuosi");
+  nct_add_coord(&tallenn, NCTVAR(vset,"lat").data, NCTDIM(vset,"lat").len, NCTVAR(vset,"lat").xtype, "lat");
+  NCTVAR(vset,"lat").data = NULL,
+  nct_add_coord(&tallenn, NCTVAR(vset,"lon").data, NCTDIM(vset,"lon").len, NCTVAR(vset,"lon").xtype, "lon");
+  NCTVAR(vset,"lon").data = NULL,
+  
+  tallenn.vars = realloc(tallenn.vars, (tallenn.nvars+3)*sizeof(nct_var));
+  int dimids[] = {0,1,2};
+  char* nimet[] = {"summer","freezing","winter"};
+  for(int i=0; i<3; i++)
+    nct_simply_add_var(&tallenn, pituudet1[i], NC_SHORT, 3, dimids, nimet[i]);
+  nct_write_ncfile(&tallenn, argv[2]);
+  nct_free_vset(&tallenn);
+  nct_free_vset(&vset);
+  return 0;
+}
