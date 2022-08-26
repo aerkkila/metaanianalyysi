74de45bf5ed55fdcde2db48e332c9d9572462871
  <aerkkila@users.noreply.github.com>
2022-08-26 12:54:49 +0300

    ikir_alkuloppu tekee kaikki kaudet ja päivitetty

--- ikir_alkuloppu.py
+++ ikir_alkuloppu.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import numpy as np
-import xarray as xr
-from numpy import sin
+from netCDF4 import Dataset
 from matplotlib.pyplot import *
-import luokat
+import luokat, warnings
 from multiprocessing import Process
 
 def argumentit():
@@ -30,7 +29,8 @@ def vaihda_ikirluokka(hyppy:int):
     global ikir_ind
     ikir_ind = (ikir_ind + len(luokat.ikir) + hyppy) % len(luokat.ikir)
     if xarrlis[ikir_ind] is None:
-        xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1(paivat, paivat.where(ikirdat==ikir_ind,np.nan), tarkk)
+        maski = ikirdat==ikir_ind
+        xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1(päivät, päivät[maski], tarkk, alat[maski])
         return True
     return False
 
@@ -41,12 +41,12 @@ def vaihda_ikirluokka_piirtaen(hyppy:int):
     title(luokat.ikir[ikir_ind])
     gca().relim()
     gca().autoscale_view()
+    draw()
+    return
     if aln[0] == 's':
         xlim([-110,75])
     else:
         xlim([-20,180])
-    draw()
-    return
 
 def nappainfunk(tapaht):
     if tapaht.key == 'right' or tapaht.key == 'o' or tapaht.key == 'i':
@@ -57,34 +57,31 @@ def nappainfunk(tapaht):
 
 # Diskretisoi darr-DataArrayn tarkk-tarkkuudella ja
 # laskee paljonko pinta-alaa on kullakin syntyneellä osuudella.
-def pintaalat1x1(kokodata,darr,tarkk):
-    aste = 0.0174532925199
-    R2 = 40592558970441
-    PINTAALA = lambda _lat: aste*R2*(sin((_lat+1)*aste) - sin(_lat*aste))*1.0e-6
-    
-    dat = darr.data.flatten().astype(int)
-    minluku = int(kokodata.min())
-    maxluku = int(kokodata.max())
+def pintaalat1x1(kokodata,darr,tarkk,alat):
+    minluku = int(np.nanmin(kokodata))
+    maxluku = int(np.nanmax(kokodata))
     lukualat = np.zeros( (maxluku - minluku) // tarkk + 1 )
-    for j,la in enumerate(darr.lat.data):
-        ala = PINTAALA(la)
-        lonarr = dat[ j*darr.lon.size : (j+1)*darr.lon.size ]
-        lonarr = lonarr[lonarr >= minluku]
-        for luku in lonarr.astype(int):
-            lukualat[(luku-minluku)//tarkk] += ala
+    for i,luku in enumerate(darr):
+        if luku != luku:
+            continue
+        lukualat[(int(luku)-minluku)//tarkk] += alat[i]
     return np.arange(minluku,maxluku+1,tarkk), lukualat
 
-al_muuttuja = ['talven_alku','talven_loppu']
-al_nimi = ['start','end']
+al_muuttuja = ['jaatym_alku','talven_alku','talven_loppu']
+al_nimi = ['freezing_start', 'winter_start', 'summer_start']
 
-def aja(doy,ftnum):
-    global ikir_ind, xarrlis, yarrlis, paivat, palkit, aln
+def aja(kaudet, ftnum, ikirdat, alat):
+    global ikir_ind, xarrlis, yarrlis, päivät, palkit, aln
     ikir_ind=0
     for alm, aln in zip(al_muuttuja, al_nimi):
-        paivat = doy[alm]
+        päivät = np.ma.getdata(kaudet[alm], False)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            päivät = np.nanmean(päivät, axis=0).flatten()
         xarrlis = [None]*len(luokat.ikir)
         yarrlis = xarrlis.copy()
-        xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1(paivat, paivat.where(ikirdat==ikir_ind,np.nan), tarkk)
+        maski = ikirdat==ikir_ind
+        xarrlis[ikir_ind],yarrlis[ikir_ind] = pintaalat1x1(päivät, päivät[maski], tarkk, alat[maski])
 
         if verbose:
             import locale
@@ -97,7 +94,7 @@ def aja(doy,ftnum):
 
         fig = figure()
         palkit = bar(xarrlis[ikir_ind], yarrlis[ikir_ind]/1000, width=0.8*tarkk)
-        xlabel('winter %s, data %i' %(aln,ftnum))
+        xlabel('%s%s' %(aln.replace('_',' '), '' if ftnum<0 else ', data%i' %ftnum))
         ylabel('extent (1000 km$^2$)')
         title(luokat.ikir[ikir_ind])
         tight_layout()
@@ -107,28 +104,46 @@ def aja(doy,ftnum):
             show()
             continue
         while True:
-            savefig("kuvia/yksittäiset/ikir%i_w%s_ft%i.png" %(ikir_ind,aln,ftnum))
+            savefig("kuvia/yksittäiset/ikir%i_%s%s.png" %(ikir_ind,aln,
+                                                          '' if ftnum<0 else '_ft%i' %ftnum))
             if ikir_ind+1 == len(luokat.ikir):
                 ikir_ind = 0
                 break
             vaihda_ikirluokka_piirtaen(1)
 
 def main():
-    global ikirdat
+    global ikirdat, alat
     rcParams.update({'font.size':19,'figure.figsize':(12,10)})
     argumentit()
     ikir_ind = 0
     kerroin = 8
-    ftluvut = [0]
+    ftluvut = [-1]
+    ft_oletus = 2
+    avaa_kaudet = lambda l: Dataset("kausien_pituudet%i.nc" %(ft_oletus if l<0 else l))
+    ikirdat = np.ma.getdata(Dataset('ikirdata.nc')['luokka'])
+    ikirdat = np.round(np.mean(ikirdat, axis=0)).astype(int).flatten()
+    alat = np.load('pintaalat.npy')
+
+    if(len(ftluvut) == 1):
+        a = avaa_kaudet(ftluvut[0])
+        aja(a, ftluvut[0], ikirdat, alat)
+        a.close()
+        return
+
     pros = np.empty(len(ftluvut),object)
     for i,l in enumerate(ftluvut):
-        doy = xr.open_dataset("kausien_pituudet%i.nc" %l)[al_muuttuja].mean(dim='vuosi')
+        kaudet = avaa_kaudet(l)
         if(i==0):
-            ikirdat = xr.open_dataset("prfdata_avg.nc")['luokka'].sel({'lat':slice(doy.lat.min(),doy.lat.max())})
-        pros[i] = Process(target=aja, args=[doy,l])
+            ikirdat = xr.open_dataset("prfdata_avg.nc")['luokka'].sel({'lat':slice(kaudet.lat.min(),kaudet.lat.max())})
+        pros[i] = Process(target=aja, args=[kaudet, l, ikirdat, alat])
         pros[i].start()
     for p in pros:
         p.join()
+    ikirdat.close()
 
-if __name__ == '__main__':
-    main()
+if __name__=='__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('')
+        exit()
