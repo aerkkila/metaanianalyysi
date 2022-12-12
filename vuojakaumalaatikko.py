76221e305ed6efa6736a4c0d266115a1ef58c0d9
  <>
2022-12-12 04:21:33 +0200

    vuojakaumalaatikko ikir- ja köppkosteikosta

--- vuojakaumalaatikko.py
+++ vuojakaumalaatikko.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
+#!/bin/env python3
 from matplotlib.pyplot import *
 import numpy as np
 import sys, struct, os
 from laatikkokuvaaja import laatikkokuvaaja
 import luokat
 
-luokitus0 = np.array([*luokat.ikir, *luokat.kopp])
-luokitus1 = np.array(luokat.wetl)
-#luokitus = luokitus[luokitus!='wetland']
-kaudet   = ['summer','freezing','winter']
+kaudet   = luokat.kaudet[1:]
 flatjarj = 'C'
 pripost  = ['pri', 'post']
+ppnum = 0 if 'pri' in sys.argv else 1
 
 def laita_keskiarvot(avg, nimet, tunniste):
     assert(flatjarj == 'C')
@@ -18,7 +16,7 @@ def laita_keskiarvot(avg, nimet, tunniste):
     rivit = [b[0] for b in a[::len(kaudet)]]
     sarakt = [b[1] for b in a[:len(kaudet)]]
     avg = np.array(avg).reshape([len(rivit),len(sarakt)])
-    f = open('vuojakaumalaatikko_avg%s.csv' %tunniste, 'w')
+    f = open('vuojakaumalaatikko_avg_pyout%s.csv' %tunniste, 'w')
     for s in sarakt:
         f.write(',%s' %s)
     for j,r in enumerate(rivit):
@@ -28,36 +26,34 @@ def laita_keskiarvot(avg, nimet, tunniste):
     f.write('\n')
     f.close()
 
-def aja(luokitus,tunniste):
+def lue(luokitus, kost):
     dt     = np.empty([len(luokitus),len(kaudet)], object)
     nimet  = np.empty([len(luokitus),len(kaudet)], object)
-    pitdet = np.empty([len(luokitus),len(kaudet)], np.int32)
-    ppnum = 0 if 'pri' in sys.argv else 1
     for luokkai,luokka in enumerate(luokitus):
+        dir = "vuojakaumadata%s" %("/kost" if kost[luokkai] else "")
         for kausii,kausi in enumerate(kaudet):
-            with open('vuojakaumadata/%s_%s_%s.bin' %(luokka,kausi,pripost[ppnum]), "r") as f:
+            with open('%s/%s_%s_%s.bin' %(dir, luokka, kausi, pripost[ppnum]), "r") as f:
                 raaka = f.buffer.read()
-            pit2 = struct.unpack("ii", raaka[:8])
-            dt    [luokkai, kausii] = np.ndarray(pit2[0], dtype=np.float32, buffer=raaka[4:])
+            pit2 = struct.unpack("i", raaka[:4])[0]
+            dt    [luokkai, kausii] = np.ndarray(pit2, dtype=np.float32, buffer=raaka[4:])
             nimet [luokkai, kausii] = "%s, %s" %(luokka,kausi)
-            pitdet[luokkai, kausii] = pit2[1]
+    return dt, nimet
 
-    varit = 'rkb'*dt.shape[0]
-    varit = [*varit]
-    tulos = laatikkokuvaaja(dt.flatten(order=flatjarj), fliers='', vari=varit, avgmarker='.')
+def aja(dt, nimet, tunniste):
+    värit = 'rkb'*dt.shape[0]
+    värit = [*värit]
+    tulos = laatikkokuvaaja(dt.flatten(order=flatjarj), fliers='', vari=värit, avgmarker='.')
     laita_keskiarvot(tulos['avg'], nimet, tunniste)
     xticks(tulos['xsij'], labels=[s.replace('_',' ') for s in nimet.flatten(order=flatjarj)], rotation=90, ha='center')
     ax = gca()
-    #[t.set_color(v) for t,v in zip(ax.xaxis.get_ticklabels(), varit)]
-    #[t.set_color(v) for t,v in zip(ax.xaxis.get_ticklines(), varit)]
+    #[t.set_color(v) for t,v in zip(ax.xaxis.get_ticklabels(), värit)]
+    #[t.set_color(v) for t,v in zip(ax.xaxis.get_ticklines(), värit)]
     grid('on', axis='y')
     ylabel('nmol m$^{-2}$ s$^{-1}$')
     yticks(rotation=90)
-#    for i,x in enumerate(tulos['xsij'][:-1]):
-#        text(x, 1.03, "%i" %(pitdet.flatten(order=flatjarj)[i]), transform=ax.get_xaxis_transform(), rotation=90, ha='center')
     tight_layout()
     if '-s' in sys.argv:
-        nimi = 'kuvia/%s_%s_%s.png' %(sys.argv[0][:-3], pripost[ppnum], tunniste)
+        nimi = 'kuvia/vuojakaumalaatikko_%s_%s.png' %(pripost[ppnum], tunniste)
         savefig(nimi)
         clf()
         os.system('gm convert -rotate 90 %s %s' %(nimi,nimi))
@@ -65,10 +61,25 @@ def aja(luokitus,tunniste):
         show()
 
 def main():
-    rcParams.update({'figure.figsize':(len(luokitus0)*1.3,11), 'font.size':16})
-    aja(luokitus0, '0')
-    rcParams.update({'figure.figsize':(len(luokitus1)*1.3,11), 'font.size':16})
-    aja(luokitus1, '1')
+    def params():
+        rcParams.update({'figure.figsize':(len(luokitus)*1.3,11), 'font.size':16})
+
+    luokitus = np.array([*luokat.ikir, *luokat.kopp])
+    params()
+    dt,nimet = lue(luokitus, np.zeros(len(luokitus), bool))
+    aja(dt, nimet, '0')
+
+    luokitus = np.array([*luokitus, 'fen', 'tundra_wetland'])
+    kost = np.ones(len(luokitus), bool)
+    kost[-2:] = False
+    params()
+    dt,nimet = lue(luokitus, kost)
+    aja(dt, nimet, '1')
+
+    luokitus = np.array(luokat.wetl)
+    rcParams.update({'figure.figsize':(len(luokitus)*1.3,11), 'font.size':16})
+    dt, nimet = lue(luokitus, np.zeros(len(luokitus), bool))
+    aja(dt, nimet, '2')
 
 if __name__=='__main__':
     try:
