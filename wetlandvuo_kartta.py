dd6aac2db306e174698cd496eec25669c0e79d1f
  <>
2022-12-19 12:45:07 +0200

    wetlandvuo_kartta.py lisätty

---
+++ wetlandvuo_kartta.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+from netCDF4 import Dataset
+import numpy as np
+from matplotlib.pyplot import *
+import cartopy.crs as ccrs
+
+def tee_data():
+    ds = Dataset('flux1x1.nc', 'r')
+    vuo = np.ma.getdata(ds['flux_bio_posterior'][:])
+    vuo = np.mean(vuo, axis=0)
+    lat = np.ma.getdata(ds['lat'])
+    lon = np.ma.getdata(ds['lon'])
+    ds.close
+
+    ds = Dataset('BAWLD1x1.nc')
+    wet = np.ma.getdata(ds['wetland'][:])
+    ds.close
+
+    vuo = np.where(wet<0.05, np.nan, vuo)
+    return vuo, lat, lon
+
+def main():
+    rcParams.update({'font.size': 15,
+                     'figure.figsize': (12,10)})
+    vuo,lat,lon = tee_data()
+    lon,lat = np.meshgrid(lon,lat, sparse=True)
+
+    platecarree = ccrs.PlateCarree()
+    projektio   = ccrs.LambertAzimuthalEqualArea(central_latitude=90)
+    kattavuus   = [-180,180,45,90]
+    ax = axes(projection=projektio)
+    ax.set_extent(kattavuus, platecarree)
+    ax.coastlines()
+
+    pcolormesh(lon, lat, vuo, transform=platecarree, cmap=get_cmap('gnuplot2_r'))
+    colorbar()
+    tight_layout()
+    show()
+
+if __name__=='__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('')
+        exit()
