d0c1a7fdb63fe9df3df865e6f10550bc23bac5b6
  <>
2022-12-23 15:18:42 +0200

    vuosijainnit, mutta ei toimi

---
+++ vuosijainnit.c
@@ -0,0 +1,101 @@
+#include <nctietue2.h>
+#include <stdlib.h>
+
+#define VIRHE -1234567
+int montako_päivää(time_t aika0, int vuosi, float fpäivä) {
+    if(fpäivä != fpäivä)
+	return VIRHE;
+    struct tm aikatm = {
+	.tm_year = vuosi-1900,
+	.tm_mon  = 0,
+	.tm_mday = 1+(int)fpäivä,
+    };
+    time_t kohdeaika = mktime(&aikatm);
+    return (kohdeaika - aika0) / 86400;
+}
+
+struct tiedot {
+    double *vuo, *alut, *loput, WET, wet;
+    int *vuodet, vuosia, res;
+    time_t aika0;
+};
+
+float pisteen_vuo(const struct tiedot* restrict tiedot) {
+    int n = 0;
+    double summa = 0;
+    for(int i=0; i<tiedot->vuosia; i++) {
+	int alku  = montako_päivää(tiedot->aika0, tiedot->vuodet[i], tiedot->alut[i*tiedot->res]),
+	    loppu = montako_päivää(tiedot->aika0, tiedot->vuodet[i], tiedot->loput[i*tiedot->res]);
+	if(alku == VIRHE || loppu == VIRHE)
+	    continue;
+	for(int t=alku; t<loppu; t++)
+	    summa += tiedot->vuo[t*tiedot->res] / tiedot->WET * tiedot->wet;
+	n += loppu-alku;
+    }
+    return summa*1e9 / n;
+}
+
+char* luo_alue(const double* prfwet, const double* wet, char* alue, int xyres) {
+    for(int i=0; i<xyres; i++)
+	alue[i] = alue[i] && wet[i] >= 0.05 && prfwet[i] < 0.97 && prfwet[i] > 0.03;
+    return alue;
+}
+
+const char* luokat[] = {"bog", "fen", "marsh", "permafrost_bog", "tundra_wetland"};
+const int luokkia = sizeof(luokat)/sizeof(char*);
+
+int main() {
+    nct_vset *aluevset = nct_read_ncfile("aluemaski.nc"),
+	     *bawvset  = nct_read_ncfile_info("BAWLD1x1.nc"),
+	     *vuovset  = nct_read_ncfile_info("flux1x1.nc"),
+	     *kauvset  = nct_read_ncfile_info("kausien_päivät.nc");
+
+    double *WET   = nct_load_data_with(bawvset, "wetland",            nc_get_var_double, sizeof(double)),
+	   *vuo   = nct_load_data_with(vuovset, "flux_bio_posterior", nc_get_var_double, sizeof(double)),
+	   *alut  = nct_load_data_with(kauvset, "summer_start",       nc_get_var_double, sizeof(double)),
+	   *loput = nct_load_data_with(kauvset, "summer_end",         nc_get_var_double, sizeof(double));
+    int    *vuodt = nct_load_data_with(kauvset, "vuosi",              nc_get_var_int,    sizeof(int));
+
+    nct_var* maski = nct_next_truevar(aluevset->vars[0], 0);
+    double* prfwet = nct_load_data_with(bawvset, "wetland_prf", nc_get_var_double, sizeof(double));
+    int xyres = nct_get_varlen(maski);
+    char* alue = luo_alue(prfwet, WET, maski->data, xyres);
+    
+    struct tiedot tiedot = {
+	.vuodet = vuodt,
+	.vuosia = 2020 - vuodt[0],
+	.res    = xyres,
+	.aika0  = nct_mktime0(nct_get_var(vuovset, "time"), NULL).a.t,
+    };
+
+    nct_vset tallenn = {0};
+    nct_copy_var(&tallenn, nct_get_var(aluevset, "lat"), 1);
+    nct_copy_var(&tallenn, nct_get_var(aluevset, "lon"), 1);
+    int varid[] = {0,1};
+    
+    for(int j=0; j<luokkia; j++) {
+	float *uusidata = malloc(xyres*sizeof(float));
+        double *wet   = nct_load_data_with(bawvset, luokat[j], nc_get_var_double, sizeof(double));
+
+	for(int i=0; i<xyres; i++) {
+	    if(!alue[i]) {
+		uusidata[i] = 0.0f / 0.0f;
+		continue; }
+	    tiedot.wet   = wet[i];
+	    tiedot.WET   = WET[i];
+	    tiedot.vuo   = vuo+i;
+	    tiedot.alut  = alut+i;
+	    tiedot.loput = loput+i;
+	    uusidata[i] = pisteen_vuo(&tiedot);
+	}
+
+	nct_add_var(&tallenn, uusidata, NC_FLOAT, (char*)luokat[j], 2, varid);
+    }
+    nct_write_ncfile(&tallenn, "vuosijainnit.nc");
+
+    nct_free_vset(&tallenn);
+    nct_free_vset(aluevset);
+    nct_free_vset(bawvset);
+    nct_free_vset(vuovset);
+    nct_free_vset(kauvset);
+}
