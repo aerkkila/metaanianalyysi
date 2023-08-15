44d931b40265edd211db7b597d096d44345f74f1
  <>
2023-08-15 17:54:17 +0300

    curl vaati kauttamerkin urlin perään

--- create_ikirdata_using_a_landmask/lataa.sh
+++ create_ikirdata_using_a_landmask/lataa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p raaka
-osoite=https://dap.ceda.ac.uk/neodc/esacci/permafrost/data/permafrost_extent/L4/area4/pp/v03.0
+osoite=https://dap.ceda.ac.uk/neodc/esacci/permafrost/data/permafrost_extent/L4/area4/pp/v03.0/
 nimet=`curl -s $osoite -L |grep -oP '(?<=href=")[^"]*(?=")' |grep "^.*\.nc$"`
 [ $? != 0 ] && echo curl epäonnistui. Lieneekö nettiä?
 for n in $nimet; do
